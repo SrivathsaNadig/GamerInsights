f73bfb11de5649898bbbec53d1fb041b0f770430
Srivathsa N <t1jit21cse2100006@jyothyit.ac.in>
2025-03-07 09:51:06 +0530

    Finished Basic Analysis

---
+++ Scripts/GamerInsights Quick Access Tables.sql
@@ -0,0 +1,27 @@
+--GAME AND PLAYERS SESSION
+SELECT 
+    u.[UserID], 
+    [UserName], 
+    [Country], 
+    [Age], 
+    [Gender], 
+    g.[GameID], 
+    [GameName], 
+    [RegistrationDate],
+    [Platform],
+    [AchievementsUnlocked],
+    [LastLogin],
+    [TotalPlayTime],
+    [SessionID], 
+    [SessionStart], 
+    [SessionEnd], 
+    [SessionLength],
+    [SessionType], 
+    [LevelReached], 
+    [ReleaseDate]
+INTO #quickaccess
+FROM Users u 
+LEFT JOIN Sessions s ON u.UserID = s.UserID
+RIGHT JOIN Games g ON g.GameID = s.GameID;
+
+

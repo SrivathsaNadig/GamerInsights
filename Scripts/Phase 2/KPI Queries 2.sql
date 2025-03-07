19cfe28b23b63316c4003eaebd1a7c29e8e646fe
Srivathsa N <t1jit21cse2100006@jyothyit.ac.in>
2025-03-07 19:34:20 +0530

    Retention Rate Query To Be Finished 2025-03-07 19:33:37.867

--- Scripts/Phase 2/KPI Queries 2.sql
+++ Scripts/Phase 2/KPI Queries 2.sql
@@ -20,3 +20,38 @@ select
 from [sessions] s 
 group by datetrunc(MONTH,s.session_start)
 order by 1;
+
+--Average Session Length
+select avg(datediff(HOUR,session_start,session_end)) 'Average Session Length'
+from sessions;
+
+--Level Completion Rate
+SELECT 
+	count(distinct user_id)* 100/(select count(*) from users) 'Level Completion Rate This Banner'
+FROM
+	events
+WHERE event_type = 'level_completion' ;
+
+select name as tablenname from sys.tables
+
+
+--CHURN RATE   (say 25 days)
+
+select 
+	count(datediff(day,last_active,GETDATE()))/(select count(user_id) *0.01 from users) 'churnrate'
+from users
+where 	datediff(day,last_active,GETDATE())>=25;
+
+--OR
+
+SELECT 
+	COUNT(*) * 100 /(select count(*) from users) 'Churnrate'
+FROM USERS 
+	WHERE last_active < dateadd(DAY,-25,GETDATE())
+
+
+-- Retention Rate
+
+SELECT *
+FROM Sessions
+WHERE session_end > session_start;
